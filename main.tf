facfd29cb8eee5484ae46db8855a3e69e79da20b
Luca Sander <luca.sander@online.de>
2020-08-25 18:25:48 +0200

    Fixxed our version dockerfile

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ terraform {
   
 }
 
+variable "imagebuild {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
 
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
@@ -30,7 +36,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container {
         name                    = "weatherapi"
-        image                   = "sandermann/weatherapi"
+        image                   = "sandermann/weatherapi:${var.imagebuild}"
             cpu                 = "1"
             memory              = "1"
 
